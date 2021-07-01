af07ffee2d88fa683063f67b7309ec6fc0b7738b
Bhalerao, Ajay <AjayBhalerao@Eaton.com>
2021-07-01 10:00:14 +0530

    Include http call

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,5 +1,6 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
+const request = require('request');
 let response;
 
 /**
@@ -15,19 +16,33 @@ let response;
  * 
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
+    // try {
+    //     // const ret = await axios(url);
+    //     response = {
+    //         'statusCode': 200,
+    //         'body': JSON.stringify({
+    //             message: 'hello world',
+    //             // location: ret.data.trim()
+    //         })
+    //     }
+    // } catch (err) {
+    //     console.log(err);
+    //     return err;
+    // }
+
+    try{
+
+        
+        request.get('https://jsonplaceholder.typicode.com/todos/1', function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        return response.json();    
+    });
+    }catch (err) {
         console.log(err);
         return err;
     }
 
-    return response
+    // return response
 };
